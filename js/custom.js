762684cea3d9966df78464c4cf5f6a2a07a2cd49
Bonku <bonkurockzzz@gmail.com>
2022-11-14 14:08:47 +0530

    new addition

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,10 @@ $(document).ready(function(){
         $(this).hide();
         $('.contact-form').show('slow');
     });
-    
+    $('.close-form').click(function() {
+        $('.contact-form').hide();
+        $('.contact-form-trigger').show('slow');
+    })
     $('.more-text').click(function(){
         $('.hidden-text').toggleClass('visited');
         if ($(this).text() == "Show More +")
@@ -34,25 +37,55 @@ $(window).on('load', function () {
     });
 });
 
+let contactForm = document.getElementById('contact-form').onsubmit = function() {sendEmail(title, email, message)};
+
 function sendEmail(title, email, message) {
+
     var title= $('#name').val();
     var email= $('#email').val();
-    var message= $('#massage').val();
-    var body='Name:'+title+ 'Email:'+email+ 'Message:'+message;
+    var message= $('#message').val();
+    var phoneNumber = $('#phone-number').val();
+    var body='Name: '+title+ '<br>Phone Number: '+phoneNumber+'<br>Email: '+email+ '<br>Message: '+message;
 
     Email.send({
-      Host: "smtp.gmail.com",
-      Username: "rudrapaul423@gmail.com",
-      Password: "vcwawrlmnfigktwq",
-      To: 'sankarpaul6789@gmail.com',
-      From: "rudrapaul423@gmail.com",
-      Subject: "New Massage Form Website",
-      Body: body,
+        SecureToken:"64d22219-762d-495d-8bc3-f01aaf2fccaa",
+        Host: "smtp.elasticemail.com",
+        Username: "colorshotpm@gmail.com",
+        Password: "04B01BD40820F307ECEDB9211DB1303AB161",
+        To: 'bonkurockzzz@gmail.com',
+        From: "colorshotpm@gmail.com",
+        Subject: "New Massage Form Website",
+        Body: body,
     })
     .then(function (message) {
+        window.location.reload();
+        console.log(message);
         alert("mail sent successfully");
     });
     console.log(body);
+// Email.send({
+//     SecureToken:"64d22219-762d-495d-8bc3-f01aaf2fccaa",
+//     Host : "smtp.elasticemail.com",
+//     Username : "colorshotpm@gmail.com",
+//     Password : "04B01BD40820F307ECEDB9211DB1303AB161",
+//     To: 'bonkurockzzz@gmail.com',
+//     From: "colorshotpm@gmail.com",
+//     Subject: "New message on contact from "+name,
+//     Body: body
+// }).then(
+//     message =>{
+//         //console.log (message);
+//         if(message=='OK'){
+//         alert('Your mail has been send. Thank you for connecting.');
+//         }
+//         else{
+//             console.error (message);
+//             alert('There is error at sending message. ')
+            
+//         }
+
+//     }
+// );
 };
 
 
